7589d28fe30062e944b9a00b19141887bc823f1a
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2025-04-12 20:03:02 +0200

    Ex2 - refactor

--- src/test/scala/ex2/Ex2Test.scala
+++ src/test/scala/ex2/Ex2Test.scala
@@ -4,12 +4,12 @@ import org.junit.*
 import org.junit.Assert.*
 
 class Ex2Test:
+  import Question.*
 
   val conferenceReviewing: ConferenceReviewing = ConferenceReviewing()
-  val someScores: Map[Question, Int] =
-    Map(Question.Relevance -> 8, Question.Significance -> 8, Question.Confidence -> 7, Question.Final -> 8)
+  val someScores: Map[Question, Int] = Map(Relevance -> 8, Significance -> 8, Confidence -> 7, Final -> 8)
 
-  @Test def getCorrectAverageFinalScores(): Unit =
+  @Test def testAverageFinalScore(): Unit =
     conferenceReviewing.loadReview(1, 8, 8, 6, 8)
     assertEquals(8.0, conferenceReviewing.averageFinalScore(1), 0.01)
     conferenceReviewing.loadReview(1, 9, 9, 6, 9)
